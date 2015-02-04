bad8b7b816a76ff6e7711639bd7e7dfd61d331fd
Peter Midford <peter.midford@gmail.com>
2015-02-04 15:05:16 -0500

    Participant Elements are used to represent something like OWL class/individual expressions in arachadmin

---
+++ src/main/java/org/arachb/arachadmin/PElementBean.java
@@ -0,0 +1,19 @@
+package org.arachb.arachadmin;
+
+import org.arachb.owlbuilder.lib.AbstractResults;
+
+public class PElementBean implements BeanBase {
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void fill(AbstractResults record) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
