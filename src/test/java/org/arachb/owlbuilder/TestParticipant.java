80f5ba14b56ed6718d0883d880a9692b98e63ed9
Peter Midford <peter.midford@gmail.com>
2015-02-22 09:41:14 -0500

    Testing for OWL generation code

---
+++ src/test/java/org/arachb/owlbuilder/TestParticipant.java
@@ -0,0 +1,66 @@
+package org.arachb.owlbuilder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.sql.SQLException;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.arachb.arachadmin.AbstractConnection;
+import org.arachb.arachadmin.ClaimBean;
+import org.arachb.arachadmin.DBConnection;
+import org.arachb.arachadmin.ParticipantBean;
+import org.arachb.owlbuilder.lib.Participant;
+import org.junit.Before;
+import org.junit.Test;
+import org.semanticweb.owlapi.model.OWLObject;
+
+public class TestParticipant {
+
+    private static Logger log = Logger.getLogger(TestParticipant.class);
+
+    private AbstractConnection testConnection;
+
+    private final int TESTCLAIMID = 1;
+    private ClaimBean testClaim;
+    
+    private Set<ParticipantBean> beanSet;
+	private Owlbuilder builder;
+	
+	@Before
+	public void setUp() throws Exception {
+		if (DBConnection.testConnection()){
+			log.info("Testing with live connection");
+			testConnection = DBConnection.getTestConnection();
+		}
+		else{
+			log.info("Testing with mock connection");
+			testConnection = DBConnection.getMockConnection();
+		}
+    	builder = new Owlbuilder();
+    	testClaim = testConnection.getClaim(TESTCLAIMID);
+		beanSet = testConnection.getParticipants(testClaim);
+    	
+	}
+
+	@Test
+	public void testParticipant() throws Exception{
+		for (ParticipantBean pb : beanSet){
+			Participant p = new Participant(pb);
+		}
+	}
+
+	@Test
+	public void testGenerateOWL() throws Exception {
+		for (ParticipantBean pb : beanSet){
+			pb.loadElements(testConnection);
+			pb.traverseElements();
+			Participant p = new Participant(pb);
+			OWLObject o = p.generateOWL(builder);
+			//assertNotNull(o);
+		}
+	}
+
+}
