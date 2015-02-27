2c3b2e94cff7e4864f2cb22a4a949a54553588e5
Peter Midford <peter.midford@gmail.com>
2015-02-27 15:32:10 -0500

    switch from AbstractNamedEntity to GeneratingEntity (with appropriate method switches)

--- src/main/java/org/arachb/owlbuilder/lib/Taxon.java
+++ src/main/java/org/arachb/owlbuilder/lib/Taxon.java
@@ -8,7 +8,7 @@ import org.arachb.arachadmin.TaxonBean;
 import org.arachb.owlbuilder.Owlbuilder;
 import org.semanticweb.owlapi.model.OWLObject;
 
-public class Taxon implements AbstractNamedEntity{
+public class Taxon implements GeneratingEntity{
 
 	private static Logger log = Logger.getLogger(Taxon.class);
 	private final TaxonBean bean;
@@ -17,11 +17,6 @@ public class Taxon implements AbstractNamedEntity{
 		bean = b;
 	}
 
-	@Override
-	public int getId() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
 	@Override
 	public OWLObject generateOWL(Owlbuilder b) throws Exception {
@@ -29,42 +24,13 @@ public class Taxon implements AbstractNamedEntity{
 		return null;
 	}
 
-	@Override
-	public void setGeneratedId(String id) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public String getGeneratedId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public String getIriString() {
-		return bean.getIriString();
-	}
-
-	@Override
-	public String checkIriString() {
-		if (bean.get_available_id() == null){
-			throw new IllegalStateException("Term has neither assigned nor generated id");
-		}
-		else {
-			return bean.get_available_id();
-		}
-	}
 
 	public String getParentSourceId(){
 		return bean.getParentSourceId();
 	}
 
-	@Override
-	public void updateDB(AbstractConnection c) throws SQLException {
-		// TODO Auto-generated method stub
-		
-	}
 
 	public String getName() {
 		return bean.getName();
