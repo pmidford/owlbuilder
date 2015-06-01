d0476eeb8e978479ad603f55a8e11ba67a2aa81d
Peter Midford <peter.midford@gmail.com>
2015-06-01 08:22:28 -0400

    changed to interface - only two methods that aren't static remain - may want to reconsider an abstract class with all abstract methods

--- src/main/java/org/arachb/arachadmin/CachingBean.java
+++ src/main/java/org/arachb/arachadmin/CachingBean.java
@@ -1,28 +1,15 @@
 package org.arachb.arachadmin;
 
-import java.util.HashMap;
-import java.util.Map;
 
 
-public abstract class CachingBean implements BeanBase {
+public interface CachingBean  {
 
-	private static final Map<Integer, CachingBean> cache = new HashMap<Integer, CachingBean>();
-	
+	void cache();
 
-	static boolean isCached(int id){
-		return cache.containsKey(id);
-	}
-	
-	static CachingBean getCached(int id){
-		assert(cache.containsKey(id));  //shouldn't be calling this w/o checking first
-		return cache.get(id);
-	}
-
-	static void cache(CachingBean cb){
-		final int id = cb.getId();
-		cache.put(id, cb);
-	}
-
-	
+	/**
+	 * Less an update, but more a warning if multiple copies of 'cached' beans are floating
+	 * around - inconsistency is a likely outcome
+	 */
+	void updatecache();
 
 }
