089f139446b9bc2790f34c067c8afdb308a23d63
Peter Midford <peter.midford@gmail.com>
2015-06-01 08:34:53 -0400

    implement cachingBean methods and fixes to IRI processins

--- src/main/java/org/arachb/arachadmin/IndividualBean.java
+++ src/main/java/org/arachb/arachadmin/IndividualBean.java
@@ -1,6 +1,12 @@
 package org.arachb.arachadmin;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
 
 
 
@@ -9,7 +15,7 @@ import java.sql.SQLException;
  * @author pmidford
  *
  */
-public class IndividualBean extends CachingBean implements UpdateableBean{
+public class IndividualBean implements BeanBase,UpdateableBean,CachingBean{
 	
 	static final int DBID = 1;
 	static final int DBSOURCEID = 2;
@@ -17,14 +23,18 @@ public class IndividualBean extends CachingBean implements UpdateableBean{
 	static final int DBLABEL= 4;
 	static final int DBTERM = 5;
 
-	
+	private static final Map<Integer, IndividualBean> cache = new HashMap<>();
+	private static Logger log = Logger.getLogger(IndividualBean.class);
+
+
 	private int id;
 	private String source_id;
 	private String generated_id;
 	private String label;
 	private int term;  //the individual's class
-	
-	
+
+	private Set<Integer> narratives = new HashSet<>();
+
 
 	@Override
 	public void fill(AbstractResults record) throws SQLException {
@@ -63,13 +73,21 @@ public class IndividualBean extends CachingBean implements UpdateableBean{
 		
 	}
 
-	final static String INDBADDOIGENID = "Individual has neither source id nor generated id";
+	public Set<Integer> getNaratives(){
+		return narratives;
+	}
+
+	void addNarrative(int narrative_id){
+		narratives.add(narrative_id);
+	}
+
+	final static String INDBADDOIGENID = "Individual has neither source id nor generated id: ";
 
 	@Override
 	public String getIRIString() {
 		if (getSourceId() == null){
 			if (getGeneratedId() == null){
-				throw new IllegalStateException();
+				throw new IllegalStateException(INDBADDOIGENID + getId());
 			}
 			return getGeneratedId();
 		}
@@ -78,16 +96,57 @@ public class IndividualBean extends CachingBean implements UpdateableBean{
 
 	@Override
 	public String checkIRIString(IRIManager manager) throws SQLException{
-		if (getGeneratedId() == null){
-			manager.generateIRI(this);
+		if (getSourceId() == null){
+			if (getGeneratedId() == null){
+				manager.generateIRI(this);
+			}
+			return getGeneratedId();
 		}
-		return getGeneratedId();
+		return getSourceId();
 	}
 
 	@Override
 	public void updateDB(AbstractConnection c) throws SQLException {
 		// TODO Auto-generated method stub
 		
+	}
+
+	/**
+	 * may not be needed, but if we ever need to reopen a database
+	 */
+	static void flushCache(){
+		cache.clear();
+	}
+
+	public static boolean isCached(int id){
+		return cache.containsKey(id);
+	}
+
+	public static IndividualBean getCached(int id){
+		assert cache.containsKey(id) : String.format("no cache entry for %d",id);
+		return cache.get(id);
+	}
+
+	@Override
+	public void cache(){
+		if (isCached(getId())){
+			log.warn(String.format("Tried multiple caching of %s with id %d",
+					               getClass().getSimpleName(),
+					               getId()));
+		}
+		cache.put(getId(), this);
+	}
+
+
+	@Override
+	public void updatecache(){
+		CachingBean existing = cache.get(getId());
+		if (!this.equals(existing)){
+			log.warn(String.format("Forcing update of cached bean %s with id %d",
+					               getClass().getSimpleName(),
+					               getId()));
+			cache.put(getId(), this);
+		}
 	}
 
 }
