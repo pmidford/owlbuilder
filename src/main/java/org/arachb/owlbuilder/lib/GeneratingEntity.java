97937d633930d04cd86a4a220afb8ffa8be46ef0
Peter Midford <peter.midford@gmail.com>
2015-06-01 09:11:22 -0400

    add alternative method that includes a generation cache table

--- src/main/java/org/arachb/owlbuilder/lib/GeneratingEntity.java
+++ src/main/java/org/arachb/owlbuilder/lib/GeneratingEntity.java
@@ -1,5 +1,7 @@
 package org.arachb.owlbuilder.lib;
 
+import java.util.Map;
+
 import org.arachb.owlbuilder.Owlbuilder;
 import org.semanticweb.owlapi.model.OWLObject;
 
@@ -9,6 +11,18 @@ public interface GeneratingEntity {
 	/**
 	 * Creates OWLAPI object and assertions that logically define it; assertions are
 	 * added to the target ontology retrieved from the owlbuilder parameter
+	 * @param b allows access to global ontology, factory, reasoner, etc. resources
+	 * @param elements table of indexed elements that may be used to avoid regenerating
+	 * @return OWLAPI object corresponding to an OWL representation of the Entity
+	 * @throws Exception
+	 */
+
+	public OWLObject generateOWL(Owlbuilder b, Map<String, OWLObject> elements) throws Exception;
+
+
+	/**
+	 * Version that doesn't accept table
+	 * Implementations should treat as if empty table were passed in
 	 * @param b allows access to global ontology, factory, reasoner, etc. resources
 	 * @return OWLAPI object corresponding to an OWL representation of the Entity
 	 * @throws Exception
