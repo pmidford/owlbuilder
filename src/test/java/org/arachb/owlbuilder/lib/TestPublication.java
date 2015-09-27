fcd0fd1cbdcd067262b65cb16ff440fc9b48ab54
Peter Midford <peter.midford@gmail.com>
2015-09-27 19:54:31 -0400

    initial commit, but uses older model of actual files for each pub, rather than dynamically generated pages and jsons

---
+++ src/test/java/org/arachb/owlbuilder/lib/TestPublication.java
@@ -0,0 +1,85 @@
+package org.arachb.owlbuilder.lib;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestPublication {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPublication() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testHasGeneratedLabel() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetGeneratedLabel() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGenerateCitation() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGenerateOWLOwlbuilder() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGenerateOWLOwlbuilderMapOfStringOWLObject() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGet_ref_id() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGeneratePublicationPage() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testMake_citation() {
+	//    >>> make_citation("Darwin, Charles", 1859)                                     
+	//    'Darwin, Charles (1859)'                                                       
+	//    >>> make_citation("Midford, Peter; Arendt, Julie", 2015)                       
+	//    'Midford, Peter and Arendt, Julie (2015)'                                      
+	//    >>> make_citation("Maddison, Wayne; Midford, Peter; Otto, Sally", 2007)        
+	//    'Maddison, Wayne et al. (2007)'                                                
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testMake_citation1() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testLen() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
